d588ed98be6f8824630d4477a4e98b38fdae175f
Kamczata <kam.czernek@gmail.com>
2022-02-03 10:35:38 +0100

    small correction with IproductRepository

--- Codecool.CodecoolShop/Repository/IProductRepository.cs
+++ Codecool.CodecoolShop/Repository/IProductRepository.cs
@@ -8,7 +8,6 @@ namespace Codecool.CodecoolShop.Repository
 {
     public interface IProductRepository: IRepository<Product>
     {
-        public List<Product> GetAll2();
         public List<Product> GetProductsByCategory(int categoryId);
 
         public List<Product> GetProductsBySupplier(int supplierId);
